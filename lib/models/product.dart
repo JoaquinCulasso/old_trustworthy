040561f53cbcc3497673bdedd3839037da59c42a
Joaquin Culasso <joaquin.culasso@gmail.com>
2020-05-17 18:53:42 -0300

    add model product

---
+++ lib/models/product.dart
@@ -0,0 +1,53 @@
+class Product {
+
+  String name, price, label, unit, image;
+
+    Product(this.name, this.price, this.label, this.unit, this.image);
+    
+  // int _id;
+  // String _name;
+  // String _label;
+  // double _price;
+  // String _image;
+
+  // //CONSTRUCTOR
+  // Product(this._id, this._name, this._label, this._price, this._image);
+
+  // //SETTERS
+  // set id(int id) {
+  //   this._id = id;
+  // }
+
+  // set name(String name) {
+  //   this._name = name;
+  // }
+
+  // set label(String label) {
+  //   this._label = label;
+  // }
+
+  // set price(double price) {
+  //   this._price = price;
+  // }
+
+  // set image(String image) {
+  //   this._image = image;
+  // }
+
+  // //GETTERS
+  // int get id => this._id;
+  // String get name => this._name;
+  // String get label => this._label;
+  // double get price => this._price;
+  // String get image => this._image;
+
+  // Map<String, dynamic> toMap() {
+  //   return {
+  //     'id': _id,
+  //     'name': _name,
+  //     'label': _label,
+  //     'price': _price,
+  //     'image': _image
+  //   };
+  // }
+}
