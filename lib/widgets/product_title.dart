6538db6ba0e49d53b49b19d05cf58937516668d3
Joaquin Culasso <joaquin.culasso@gmail.com>
2020-06-01 19:55:23 -0300

    title of product

---
+++ lib/widgets/product_title.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class ProductTitle extends StatelessWidget {
+  final String title;
+
+  const ProductTitle({Key key, this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+      child: Row(
+        children: <Widget>[
+          Text(
+            title,
+            style: TextStyle(
+              fontSize: 25.0,
+              color: Colors.pink,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
