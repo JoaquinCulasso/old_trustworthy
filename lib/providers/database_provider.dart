140ae2be95f831d6a769b312734b0e72d3265411
Joaquin Culasso <joaquin.culasso@gmail.com>
2020-08-05 21:07:16 -0300

    CRUD database data

--- lib/providers/database_provider.dart
+++ lib/providers/database_provider.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+// import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/foundation.dart';
@@ -9,6 +10,11 @@ import 'package:old_trustworthy/models/product.dart';
 import 'package:old_trustworthy/state/database_state.dart';
 
 class DatabaseProvider with ChangeNotifier {
+  //list of data base
+  final List _productList = [];
+
+  List get productList => this._productList;
+
   //referende Storage
   final StorageReference _imageRef =
       FirebaseStorage.instance.ref().child("Vieja_Confiable");
@@ -22,6 +28,7 @@ class DatabaseProvider with ChangeNotifier {
 
   DatabaseState get databaseState => this._databaseState;
 
+  //load pruduct to database
   Future<void> loadProduct(File productImage, String name, String price,
       String unit, String category, context) async {
     _databaseState.loading();
@@ -40,9 +47,11 @@ class DatabaseProvider with ChangeNotifier {
                 duration: Duration(seconds: 8),
                 content: Text(_databaseState.lastError)));
           });
+    getDataOfDatabase();
     notifyListeners();
   }
 
+  //update product to database
   Future<void> updateProduct(
       Product product, File productImage, BuildContext context) async {
     String url;
@@ -61,17 +70,53 @@ class DatabaseProvider with ChangeNotifier {
           product.unit, product.category);
     }
 
-    //ver la navegacion
     _databaseState.isLoaded
-        ? Navigator.of(context).pushReplacementNamed('/modifyDelete')
-        : WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
-            Scaffold.of(context).showSnackBar(SnackBar(
-                duration: Duration(seconds: 8),
-                content: Text(_databaseState.lastError)));
-          });
+        ? Navigator.of(context).pop() //popAndPushNamed('/modifyDelete')
+        : _snackBarError(context);
+    // WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+    //     Scaffold.of(context).showSnackBar(SnackBar(
+    //         duration: Duration(seconds: 8),
+    //         content: Text(_databaseState.lastError)));
+    //   });
+    getDataOfDatabase();
     notifyListeners();
   }
 
+  //delete product to database
+  Future<void> deleteProduct(Product product, BuildContext context) async {
+    _databaseState.loading();
+    notifyListeners();
+
+    await deleteImageStorage(product);
+
+    if (!_databaseState.hasError) {
+      await deleteDataOfDatabase(product);
+    }
+    if (!_databaseState.hasError) {
+      _productList.remove(product);
+    }
+
+    _databaseState.isLoaded
+        ? Navigator.of(context).pop()
+        : _snackBarError(context);
+    // WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+    //     Scaffold.of(context).showSnackBar(SnackBar(
+    //         duration: Duration(seconds: 8),
+    //         content: Text(_databaseState.lastError)));
+    //   });
+    notifyListeners();
+  }
+
+  void _snackBarError(BuildContext context) {
+    Navigator.of(context).pop();
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+      Scaffold.of(context).showSnackBar(SnackBar(
+          duration: Duration(seconds: 8),
+          content: Text(_databaseState.lastError)));
+    });
+  }
+
+  //delete image of storage
   Future<void> deleteImageStorage(Product product) async {
     try {
       await _imageRef
@@ -83,7 +128,25 @@ class DatabaseProvider with ChangeNotifier {
     }
   }
 
-  //load image storage
+  //delete data of database
+  Future<void> deleteDataOfDatabase(Product product) async {
+    try {
+      _databaseRef
+          .orderByChild('image')
+          .equalTo(product.image)
+          .onChildAdded
+          .listen((event) {
+        _databaseRef.child(event.snapshot.key).remove();
+      });
+      await Future.delayed(Duration(milliseconds: 6000));
+
+      _databaseState.loaded();
+    } catch (lastError) {
+      _databaseState.error('Error eliminando imagen: ' + lastError.toString());
+    }
+  }
+
+  //load image to storage
   Future<String> loadImageStorage(File productImage) async {
     String url;
 
@@ -102,7 +165,7 @@ class DatabaseProvider with ChangeNotifier {
     return url;
   }
 
-  //load data
+  //load data to database
   Future<void> loadDataDb(String _url, String _name, String _price,
       String _unit, String _category) async {
     var dbTimeKey = DateTime.now();
@@ -133,6 +196,7 @@ class DatabaseProvider with ChangeNotifier {
     }
   }
 
+  //update data of db
   Future<void> updateDataDb(String _newUrl, String _oldUrl, String _name,
       String _price, String _unit, String _category) async {
     // update post (image, name, price, unit, category, date, time)
@@ -167,22 +231,38 @@ class DatabaseProvider with ChangeNotifier {
       _databaseState
           .error('Error actualizando datos productos: ' + lastError.toString());
     }
+  }
 
-    // _databaseRef
-    //     .orderByChild('image')
-    //     .equalTo(_url)
-    //     .onChildAdded
-    //     .listen((event) {
+  //constructor
+  DatabaseProvider() {
+    getDataOfDatabase();
+  }
 
-    //   FirebaseDatabase.instance
-    //       .reference()
-    //       .child('Vieja_Confiable')
-    //       .child(event.snapshot.key)
-    //       .update(data)
-    //       .then((value) => value)
-    //       .whenComplete(() => _databaseState.loaded())
-    //       .catchError((lastError) => _databaseState.error(
-    //           'Error actualizando datos productos: ' + lastError.toString()));
-    // });
+  //get data of database
+  void getDataOfDatabase() async {
+    try {
+      await _databaseRef.once().then((DataSnapshot snapshot) {
+        var keys = snapshot.value.keys;
+        var data = snapshot.value;
+        _productList.clear();
+
+        for (var individualKey in keys) {
+          Product products = Product(
+              data[individualKey]['name'],
+              data[individualKey]['price'],
+              data[individualKey]['category'],
+              data[individualKey]['unit'],
+              data[individualKey]['image']);
+
+          _productList.add(products);
+        }
+        _productList.length;
+      });
+    } catch (lastError) {
+      _databaseState
+          .error('Error actualizando datos productos: ' + lastError.toString());
+    }
+
+    notifyListeners();
   }
 }
