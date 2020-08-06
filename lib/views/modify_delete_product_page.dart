3ae7f9527792e13f227531a955da75df3cb0adcd
Joaquin Culasso <joaquin.culasso@gmail.com>
2020-08-05 21:08:46 -0300

    management state database with provider

--- lib/views/modify_delete_product_page.dart
+++ lib/views/modify_delete_product_page.dart
@@ -1,11 +1,10 @@
-import 'package:firebase_database/firebase_database.dart';
-import 'package:firebase_storage/firebase_storage.dart';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import 'package:old_trustworthy/models/product.dart';
+import 'package:old_trustworthy/providers/database_provider.dart';
 import 'package:old_trustworthy/views/product_update_form_page.dart';
+import 'package:provider/provider.dart';
 
 class ModifyDeleteProductPage extends StatefulWidget {
   @override
@@ -14,120 +13,137 @@ class ModifyDeleteProductPage extends StatefulWidget {
 }
 
 class _ModifyDeleteProductPageState extends State<ModifyDeleteProductPage> {
-  List productList = [];
-//TODO ver q no se refresca cuando vuelve de modificar a la vista de lista
-  @override
-  void initState() {
-    super.initState();
+  // List productList = [];
 
-    DatabaseReference productRef =
-        FirebaseDatabase.instance.reference().child('Vieja_Confiable');
+  // @override
+  // void initState() {
+  //   super.initState();
 
-    productRef.once().then((DataSnapshot snapshot) {
-      var keys = snapshot.value.keys;
-      var data = snapshot.value;
+  //   DatabaseReference productRef =
+  //       FirebaseDatabase.instance.reference().child('Vieja_Confiable');
 
-      productList.clear();
+  //   productRef.once().then((DataSnapshot snapshot) {
+  //     var keys = snapshot.value.keys;
+  //     var data = snapshot.value;
 
-      for (var individualKey in keys) {
-        Product products = Product(
-            data[individualKey]['name'],
-            data[individualKey]['price'],
-            data[individualKey]['category'],
-            data[individualKey]['unit'],
-            data[individualKey]['image']);
+  //     productList.clear();
 
-        productList.add(products);
-      }
+  //     for (var individualKey in keys) {
+  //       Product products = Product(
+  //           data[individualKey]['name'],
+  //           data[individualKey]['price'],
+  //           data[individualKey]['category'],
+  //           data[individualKey]['unit'],
+  //           data[individualKey]['image']);
 
-      setState(() {
-        print('Length: $productList.length');
-      });
-    });
-  }
+  //       productList.add(products);
+  //     }
+
+  //     setState(() {
+  //       print('Length: $productList.length');
+  //     });
+  //   });
+  // }
 
   @override
   Widget build(BuildContext context) {
+    final databaseProvider = Provider.of<DatabaseProvider>(context);
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Modificar/eliminar productos'),
         centerTitle: true,
       ),
-      body: productList.length == 0
-          ? Center(child: CircularProgressIndicator())
-          : ListView.builder(
-              itemCount: productList.length,
-              scrollDirection: Axis.vertical,
-              itemBuilder: (_, index) {
-                return Column(
-                  children: <Widget>[
-                    Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: <Widget>[
-                          Container(
-                            height: 180.0,
-                            width: 215.0,
-                            child: Column(
-                              children: <Widget>[
-                                Text(
-                                  productList[index].name,
-                                  textAlign: TextAlign.center,
-                                  style: TextStyle(
-                                    fontSize: 22.0,
-                                  ),
+      body: Builder(
+        builder: (context) => (databaseProvider.productList.length == 0
+            ? Center(child: CircularProgressIndicator())
+            : SafeArea(
+                child: ListView.builder(
+                  itemCount: databaseProvider.productList.length,
+                  scrollDirection: Axis.vertical,
+                  itemBuilder: (_, index) {
+                    return Column(
+                      children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: <Widget>[
+                              Container(
+                                height: 180.0,
+                                width: 215.0,
+                                child: Column(
+                                  children: <Widget>[
+                                    Text(
+                                      databaseProvider.productList[index]
+                                          .name, //productList[index].name,
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(
+                                        fontSize: 22.0,
+                                      ),
+                                    ),
+                                    Text(
+                                      //'\$${productList[index].price} x ${productList[index].unit}',
+                                      '\$${databaseProvider.productList[index].price} x ${databaseProvider.productList[index].unit}',
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(
+                                        fontSize: 18.0,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    Text(
+                                      //'${productList[index].category}',
+                                      '${databaseProvider.productList[index].category}',
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(
+                                        fontSize: 18.0,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    //_modifyDelProductBar(productList[index]),
+                                    _modifyDelProductBar(
+                                        databaseProvider.productList[index],
+                                        databaseProvider,
+                                        context),
+                                  ],
                                 ),
-                                Text(
-                                  '\$${productList[index].price} x ${productList[index].unit}',
-                                  textAlign: TextAlign.center,
-                                  style: TextStyle(
-                                    fontSize: 18.0,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                                Text(
-                                  '${productList[index].category}',
-                                  textAlign: TextAlign.center,
-                                  style: TextStyle(
-                                    fontSize: 18.0,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                                _modifyDelProductBar(productList[index]),
-                              ],
-                            ),
-                          ),
-                          Container(
-                            height: 150.0,
-                            width: 150.0,
-                            decoration: BoxDecoration(
-                              border: Border.all(color: Colors.black, width: 3),
-                              borderRadius: BorderRadius.circular(20),
-                              color: Colors.white,
-                              image: DecorationImage(
-                                image: NetworkImage(productList[index].image),
-                                fit: BoxFit.contain,
                               ),
-                            ),
+                              Container(
+                                height: 150.0,
+                                width: 150.0,
+                                decoration: BoxDecoration(
+                                  border:
+                                      Border.all(color: Colors.black, width: 3),
+                                  borderRadius: BorderRadius.circular(20),
+                                  color: Colors.white,
+                                  image: DecorationImage(
+                                    // image: NetworkImage(productList[index].image),
+                                    image: NetworkImage(databaseProvider
+                                        .productList[index].image),
+                                    fit: BoxFit.contain,
+                                  ),
+                                ),
+                              ),
+                            ],
                           ),
-                        ],
-                      ),
-                    ),
-                    Divider(
-                        color: Colors.greenAccent[300],
-                        thickness: 2.5,
-                        height: 10.0,
-                        indent: 15,
-                        endIndent: 15),
-                  ],
-                );
-              },
-            ),
+                        ),
+                        Divider(
+                            color: Colors.greenAccent[300],
+                            thickness: 2.5,
+                            height: 10.0,
+                            indent: 15,
+                            endIndent: 15),
+                      ],
+                    );
+                  },
+                ),
+              )),
+      ),
     );
   }
 
-  Widget _modifyDelProductBar(Product product) {
+  Widget _modifyDelProductBar(Product product,
+      DatabaseProvider databaseProvider, BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(16.0),
       child: Container(
@@ -162,7 +178,7 @@ class _ModifyDeleteProductPageState extends State<ModifyDeleteProductPage> {
                 color: Colors.white,
                 icon: Icon(CupertinoIcons.delete_simple),
                 onPressed: () {
-                  _deleteProduct(product);
+                  _deleteProduct(product, databaseProvider, context);
                 },
               ),
             ],
@@ -187,17 +203,10 @@ class _ModifyDeleteProductPageState extends State<ModifyDeleteProductPage> {
                 color: Color.fromRGBO(47, 87, 44, 1.0),
                 child: Text('Aceptar', style: TextStyle(fontSize: 20)),
                 onPressed: () {
-                  //TODO ver la nagegacion
                   setState(() {
-                    Navigator.of(context).pushAndRemoveUntil(
-                      MaterialPageRoute(
-                          builder: (context) =>
-                              ProductUpdateFormPage(product: product)),
-                      ModalRoute.withName('/modifyDelete'),
-                    );
-                    // Navigator.of(context).pushReplacement(MaterialPageRoute(
-                    //     builder: (BuildContext context) =>
-                    //         ProductUpdateFormPage(product: product)));
+                    Navigator.of(context).pushReplacement(MaterialPageRoute(
+                        builder: (BuildContext context) =>
+                            ProductUpdateFormPage(product: product)));
                   });
                 },
               ),
@@ -213,52 +222,57 @@ class _ModifyDeleteProductPageState extends State<ModifyDeleteProductPage> {
         });
   }
 
-  Future _deleteProduct(Product product) {
+  Future _deleteProduct(Product product, DatabaseProvider databaseProvider,
+      BuildContext context) {
     return showDialog(
         context: context,
         builder: (buildContext) {
           return AlertDialog(
             title: Text('Eliminar producto', style: TextStyle(fontSize: 25)),
             content: Text(
-              'Producto: ${product.name}.\nPrecio: \$${product.price}.\nUnidad: ${product.unit}.\nCategoria: ${product.category}.',
-              style: TextStyle(fontSize: 20),
-            ),
+                'Producto: ${product.name}.\nPrecio: \$${product.price}.\nUnidad: ${product.unit}.\nCategoria: ${product.category}.',
+                style: TextStyle(fontSize: 20)),
             actions: <Widget>[
               RaisedButton(
                 color: Color.fromRGBO(47, 87, 44, 1.0),
                 child: Text('Aceptar', style: TextStyle(fontSize: 20)),
                 onPressed: () {
-                  setState(() {
-                    //delete image from FirebaseStorage
-                    FirebaseStorage.instance
-                        .ref()
-                        .child("Vieja_Confiable")
-                        .getStorage()
-                        .getReferenceFromUrl(product.image)
-                        .then((value) => value.delete());
-
-                    //delete product from view list
-                    productList.remove(product);
-
-                    //delete from realtime database
-                    FirebaseDatabase.instance
-                        .reference()
-                        .child('Vieja_Confiable')
-                        .orderByChild('image')
-                        .equalTo(product.image)
-                        .onChildAdded
-                        .listen((event) {
-                      FirebaseDatabase.instance
-                          .reference()
-                          .child('Vieja_Confiable')
-                          .child(event.snapshot.key)
-                          .remove();
-                    }, onError: (Object o) {
-                      final DatabaseError error = o;
-                      print('Error: ${error.code} ${error.message}');
-                    });
-                  });
                   Navigator.of(context).pop();
+                  databaseProvider.deleteProduct(product, context);
+                  _progressAlertDialog(context);
+                  // setState(() {
+                  //delete image from FirebaseStorage
+                  // FirebaseStorage.instance
+                  //     .ref()
+                  //     .child("Vieja_Confiable")
+                  //     .getStorage()
+                  //     .getReferenceFromUrl(product.image)
+                  //     .then((value) => value.delete());
+
+                  // databaseProvider.deleteImageStorage(product);
+                  //delete product from view list
+                  // productList.remove(product);
+                  // databaseProvider.deleteDataOfDatabase(product);
+
+                  //delete from realtime database
+                  // FirebaseDatabase.instance
+                  //     .reference()
+                  //     .child('Vieja_Confiable')
+                  //     .orderByChild('image')
+                  //     .equalTo(product.image)
+                  //     .onChildAdded
+                  //     .listen((event) {
+                  //   FirebaseDatabase.instance
+                  //       .reference()
+                  //       .child('Vieja_Confiable')
+                  //       .child(event.snapshot.key)
+                  //       .remove();
+                  // }, onError: (Object o) {
+                  //   final DatabaseError error = o;
+                  //   print('Error: ${error.code} ${error.message}');
+                  // });
+                  // });
+                  // Navigator.of(context).pop();
                 },
               ),
               RaisedButton(
@@ -267,8 +281,25 @@ class _ModifyDeleteProductPageState extends State<ModifyDeleteProductPage> {
                 onPressed: () {
                   Navigator.of(context).pop();
                 },
-              )
+              ),
             ],
+          );
+        });
+  }
+
+  Future _progressAlertDialog(BuildContext context) {
+    return showDialog(
+        barrierDismissible: false,
+        context: context,
+        builder: (buildContext) {
+          return Center(
+            child: AlertDialog(
+              title: Text('Eliminando....', style: TextStyle(fontSize: 25)),
+              content: Center(
+                  heightFactor: 1.5,
+                  widthFactor: 1.5,
+                  child: CircularProgressIndicator()),
+            ),
           );
         });
   }
