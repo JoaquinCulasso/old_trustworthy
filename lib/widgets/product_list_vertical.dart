5956c512639cc3256c50d13e3ae4112f1ec2aca8
Joaquin Culasso <joaquin.culasso@gmail.com>
2020-08-29 20:57:27 -0300

    use future builder and clean code

--- lib/widgets/product_list_vertical.dart
+++ lib/widgets/product_list_vertical.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+
 import 'package:provider/provider.dart';
-import 'package:firebase_database/firebase_database.dart';
+import 'package:old_trustworthy/providers/database_provider.dart';
+import 'package:old_trustworthy/providers/shopping_cart_provider.dart';
 
 import 'package:old_trustworthy/models/product.dart';
-import 'package:old_trustworthy/providers/shopping_cart_provider.dart';
 
 class ProductListVertical extends StatefulWidget {
   final String category;
@@ -14,82 +15,105 @@ class ProductListVertical extends StatefulWidget {
 }
 
 class _ProductListState extends State<ProductListVertical> {
-  List<Product> productList = [];
-
   @override
   Widget build(BuildContext context) {
-    final ShoppingCartProvider shoppingCart =
-        Provider.of<ShoppingCartProvider>(context, listen: false);
+    final databaseProvider =
+        Provider.of<DatabaseProvider>(context, listen: false);
 
-    setState(() {
-      DatabaseReference productRef =
-          FirebaseDatabase.instance.reference().child('Vieja_Confiable');
+    return FutureBuilder(
+        future: databaseProvider.getProductListVertical(widget.category),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+              _snackBarError(context,
+                  'Error en la conexión, por favor actualice la pantalla...');
+              return LoadingData();
+            case ConnectionState.waiting:
+              return LoadingData();
+            case ConnectionState.active:
+              return LoadingData();
+            case ConnectionState.done:
+              if (databaseProvider.databaseState.hasError) {
+                _snackBarError(context,
+                    'Error buscando datos, por favor actualice la pantalla...');
+              }
+              if (!snapshot.hasData || snapshot.data.isEmpty) {
+                return LoadingData();
+              }
 
-      Query filterQuery =
-          productRef.orderByChild('category').equalTo(widget.category);
+              return ListVertical();
+          }
+          return null;
+        });
+  }
+}
 
-      filterQuery.once().then((DataSnapshot snapshot) {
-        var keys = snapshot.value.keys;
-        var data = snapshot.value;
+void _snackBarError(BuildContext context, String textError) {
+  WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+    Scaffold.of(context).showSnackBar(
+        SnackBar(duration: Duration(seconds: 3), content: Text(textError)));
+  });
+}
 
-        productList.clear();
+class LoadingData extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return IntrinsicHeight(
+      child: Container(
+        margin: EdgeInsets.symmetric(vertical: 170),
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+}
 
-        for (var individualKey in keys) {
-          Product products = Product(
-              data[individualKey]['name'],
-              data[individualKey]['price'],
-              data[individualKey]['category'],
-              data[individualKey]['unit'],
-              data[individualKey]['image']);
-
-          productList.add(products);
-        }
-        productList.length;
-        // setState(() {
-        print('Length: $productList.length');
-        // });
-      });
-    });
-
-    // final CategoryProvider categoryProvider =
-    //     Provider.of<CategoryProvider>(context);
+class ListVertical extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final databaseProvider =
+        Provider.of<DatabaseProvider>(context, listen: false);
 
     return Column(
       children: <Widget>[
-        productList.length == 0
-            ? Center(
-                child: CircularProgressIndicator(),
-              )
-            : ListView.builder(
-                shrinkWrap: true,
-                physics: ScrollPhysics(),
-                itemCount: productList.length,
-                scrollDirection: Axis.vertical,
-                itemBuilder: (context, index) {
-                  return _item(
-                      productList[index].image,
-                      productList[index].name,
-                      productList[index].category,
-                      productList[index].price,
-                      productList[index].unit,
-                      shoppingCart);
-                },
-              ),
+        ListView.builder(
+          shrinkWrap: true,
+          physics: ScrollPhysics(),
+          itemCount: databaseProvider.productListVertical.length,
+          scrollDirection: Axis.vertical,
+          itemBuilder: (context, index) {
+            return Item(
+              image: databaseProvider.productListVertical[index].image,
+              name: databaseProvider.productListVertical[index].name,
+              category: databaseProvider.productListVertical[index].category,
+              price: databaseProvider.productListVertical[index].price,
+              unit: databaseProvider.productListVertical[index].unit,
+            );
+          },
+        ),
       ],
     );
   }
+}
 
-  Widget _item(String _image, String _name, String _category, String _price,
-      String _unit, ShoppingCartProvider shoppingCart) {
+class Item extends StatelessWidget {
+  final String image, name, category, price, unit;
+
+  const Item(
+      {Key key, this.image, this.name, this.category, this.price, this.unit})
+      : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    final shoppingCart =
+        Provider.of<ShoppingCartProvider>(context, listen: false);
+
     return Column(
       children: <Widget>[
         MaterialButton(
           padding: EdgeInsets.all(0),
-          splashColor: Colors.red, // Color.fromRGBO(25, 68, 11, 0.7),
+          splashColor: Colors.red,
           onPressed: () {
-            shoppingCart.sumCounter(_price);
-            shoppingCart
-                .addCart(Product(_name, _price, _category, _unit, _image));
+            shoppingCart.sumCounter(price);
+            shoppingCart.addCart(Product(name, price, category, unit, image));
           },
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -101,14 +125,14 @@ class _ProductListState extends State<ProductListVertical> {
                 child: Column(
                   children: <Widget>[
                     Text(
-                      _name,
+                      name,
                       textAlign: TextAlign.center,
                       style: TextStyle(
                         fontSize: 22.0,
                       ),
                     ),
                     Text(
-                      '\$$_price x $_unit',
+                      '\$$price x $unit',
                       textAlign: TextAlign.center,
                       style: TextStyle(
                         fontSize: 18.0,
@@ -126,7 +150,7 @@ class _ProductListState extends State<ProductListVertical> {
                   borderRadius: BorderRadius.circular(20),
                   color: Colors.white,
                   image: DecorationImage(
-                    image: NetworkImage(_image),
+                    image: NetworkImage(image),
                     fit: BoxFit.contain,
                   ),
                 ),
@@ -143,63 +167,4 @@ class _ProductListState extends State<ProductListVertical> {
       ],
     );
   }
-
-  // Widget _item(String _image, String _name, String _category, String _price,
-  //     String _unit, BuildContext context) {
-  //   final shoppingCart = Provider.of<ShoppingCartProvider>(context);
-
-  //   return Container(
-  //     padding: EdgeInsets.symmetric(horizontal: 5.0),
-  //     child: Column(
-  //       crossAxisAlignment: CrossAxisAlignment.center,
-  //       mainAxisAlignment: MainAxisAlignment.center,
-  //       mainAxisSize: MainAxisSize.max,
-  //       children: <Widget>[
-  //         MaterialButton(
-  //           padding: EdgeInsets.all(0),
-  //           splashColor: Colors.red, // Color.fromRGBO(25, 68, 11, 0.7),
-  //           onPressed: () {
-  //             shoppingCart.sumCounter(_price);
-  //             shoppingCart
-  //                 .addCart(Product(_name, _price, _category, _unit, _image));
-  //           },
-  //           child: Container(
-  //             height: 90.0,
-  //             width: 90.0,
-  //             decoration: BoxDecoration(
-  //               color: Colors.white,
-  //               border: Border.all(color: Colors.black, width: 3),
-  //               borderRadius: BorderRadius.circular(20),
-  //               image: DecorationImage(
-  //                 image: NetworkImage(_image),
-  //                 fit: BoxFit.contain,
-  //                 alignment: Alignment.center,
-  //               ),
-  //             ),
-  //           ),
-  //         ),
-  //         Column(
-  //           children: <Widget>[
-  //             Text(
-  //               _name,
-  //               textAlign: TextAlign.center,
-  //               style: TextStyle(
-  //                 fontSize: 20,
-  //                 fontWeight: FontWeight.bold,
-  //               ),
-  //             ),
-  //             Text(
-  //               '\$$_price x $_unit',
-  //               style: TextStyle(
-  //                 fontSize: 17.5,
-  //                 // fontWeight: FontWeight.bold,
-  //               ),
-  //             ),
-  //           ],
-  //         ),
-  //       ],
-  //     ),
-  //     // ),
-  //   );
-  // }
 }
