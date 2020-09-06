86035bfe608165d07df04b66fbff0908613606e9
Joaquin Culasso <joaquin.culasso@gmail.com>
2020-09-05 21:10:43 -0300

    modify login state and preference for user

--- lib/models/account.dart
+++ lib/models/account.dart
@@ -1,14 +1,36 @@
+import 'package:old_trustworthy/configs/preference.dart';
+
 class Account {
+  //singleton
+  Account._instance();
+  static final Account instance = Account._instance();
+
   String _nameUser = '';
   String _profilePicture = '';
   String _email = '';
   String _phoneNumber = '';
 
-  void loginAccountUser(nameUser, email, profilePicture, phoneNumber) {
+  set nameUser(name) => this.nameUser = name;
+  set profilePicture(profilePicture) => this._profilePicture = profilePicture;
+  set email(email) => this._email = email;
+  set phoneNumber(phoneNumber) => this._phoneNumber = phoneNumber;
+
+  void loginAccountUser(nameUser, email, profilePicture, phoneNumber) async {
     this._nameUser = nameUser;
     this._email = email;
     this._phoneNumber = phoneNumber;
     this._profilePicture = profilePicture;
+    SharedPreferensesManager.setString('name', nameUser);
+    SharedPreferensesManager.setString('email', email);
+    SharedPreferensesManager.setString('phoneNumber', phoneNumber);
+    SharedPreferensesManager.setString('profilePicture', profilePicture);
+  }
+
+  void hasUserData() {
+    _nameUser = SharedPreferensesManager.getString('name');
+    _email = SharedPreferensesManager.getString('email');
+    _phoneNumber = SharedPreferensesManager.getString('phoneNumber');
+    _profilePicture = SharedPreferensesManager.getString('profilePicture');
   }
 
   void logoutAccountUser() {
@@ -16,6 +38,7 @@ class Account {
     this._phoneNumber = '';
     this._profilePicture = '';
     this._email = '';
+    SharedPreferensesManager.clear();
   }
 
   String get nameUser => this._nameUser;
